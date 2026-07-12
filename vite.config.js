fcc6f4cf2b4413c124cdf984479faac832e96abd
Twaran Gupta <twarangupta01@gmail.com>
2026-07-13 02:23:17 +0530

    fixed import error in viteconfig for sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePluginSitemap } from "vite-plugin-sitemap";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePluginSitemap({
+    Sitemap({
       hostname: "https://twarangupta.com",
     }),
   ],
