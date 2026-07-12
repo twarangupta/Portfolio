012e349a611dd74c8a678cc5093911de058afded
Twaran Gupta <twarangupta01@gmail.com>
2026-07-13 02:19:16 +0530

    sitemaps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePluginSitemap } from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: "https://twarangupta.com",
+    }),
+  ],
+});
